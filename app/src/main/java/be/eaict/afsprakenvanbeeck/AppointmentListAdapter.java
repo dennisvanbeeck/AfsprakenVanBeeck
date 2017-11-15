0a377a4b958f05cfe4b2e243812b4168770a95fb
dennisvanbeeck <dennis.vanbeeck@student.ap.be>
2017-11-15 15:44:44 +0100

    commit

--- app/src/main/java/be/eaict/afsprakenvanbeeck/AppointmentListAdapter.java
+++ app/src/main/java/be/eaict/afsprakenvanbeeck/AppointmentListAdapter.java
@@ -1,8 +1,11 @@
 package be.eaict.afsprakenvanbeeck;
 
 import android.content.Context;
+import android.view.ContextThemeWrapper;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
 import android.widget.TextView;
 
@@ -12,47 +15,31 @@ import java.util.List;
  * Created by dennisvanbeeck on 15/11/2017.
  */
 
-public class AppointmentListAdapter extends BaseAdapter {
-
-    private Context mContext;
-    private List<Appointment> Appointment;
+public class AppointmentListAdapter extends ArrayAdapter<Appointment> {
 
 
     //Constructor
 
-    public AppointmentListAdapter(Context mContext, List<Appointment> Appointment) {
-        this.mContext = mContext;
-        this.Appointment = Appointment;
+    public AppointmentListAdapter(Context context, List<Appointment> appointments) {
+        super(context, -1, appointments);
     }
 
-    @Override
-    public int getCount() {
-        return Appointment.size();
-    }
 
-    @Override
-    public Object getItem(int position) {
-        return Appointment.get(position);
-    }
-
-    @Override
-    public long getItemId(int position) {
-        return position;
-    }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
 
-        View v = View.inflate(mContext, R.layout.item_product_list, null);
+        LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View v = inflater.inflate(R.layout.item_product_list, null);
         TextView afspraaktijd = (TextView)v.findViewById(R.id.afspraak_tijd);
         TextView afspraakdokter = (TextView)v.findViewById(R.id.afspraak_dokter);
         TextView afspraakplaats = (TextView)v.findViewById(R.id.afspraak_plaats);
 
-        afspraakdokter.setText(Appointment.get(position).getContactName());
-        afspraaktijd.setText((int) Appointment.get(position).getTime());
-        afspraakplaats.setText(Appointment.get(position).getLocation());
+        afspraakdokter.setText(getItem(position).getContactName());
+        afspraaktijd.setText(Integer.toString(getItem(position).getHour()));
+        afspraakplaats.setText(getItem(position).getLocation());
 
-        v.setTag(Appointment.get(position).getId());
+
 
         return v;
     }
--- app/src/main/java/be/eaict/afsprakenvanbeeck/MainActivity.java
+++ app/src/main/java/be/eaict/afsprakenvanbeeck/MainActivity.java
@@ -1,11 +1,14 @@
 package be.eaict.afsprakenvanbeeck;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,22 +21,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final Integer[] arr = new Integer[26];
-        for (int i = 0; i < 10; i++) {
-            Integer integer = arr[i];
-        }
-
-
         ListView afspraakListView = (ListView) findViewById(R.id.listview_afspraak);
-        afspraakListView.setAdapter(new AppointmentListAdapter(this, arr));
-        afspraakListView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+        List<Appointment> appointments = (new AppointmentDummyRepository()).getAppointments();
+
+        afspraakListView.setAdapter(new AppointmentListAdapter(this,appointments));
+
+        /*afspraakListView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 Intent i = new Intent(MainActivity.this, DetailActivity.class);
-                i.putExtra("afspraak", arr[position]);
+                i.putExtra("afspraak", Appointment[position]);
                 startActivity(i);
             }
-        });
+        });*/
     }
 }
 
